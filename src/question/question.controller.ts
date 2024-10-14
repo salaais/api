fd69a48e1443bd782621b94c5d4b7f14a5de80b3
febreno <felipesugisawa1@gmail.com>
2024-10-14 20:00:19 -0300

    new

--- src/question/question.controller.ts
+++ src/question/question.controller.ts
@@ -14,6 +14,7 @@ import { TestGradeDto } from './dto/test-grade';
 import { GenerateTestRandomDto } from './dto/generate-test-random';
 import { QuestionBySubject } from './dto/questao-por-materia';
 
+//test
 @ApiTags('Questão')
 @Controller('questao')
 export class QuestionController {
